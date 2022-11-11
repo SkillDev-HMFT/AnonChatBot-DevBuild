d0f4857e5d4582fd1d1829a07bfc1c819f3ee283
IEatCode <68648097+IEatCodeDaily@users.noreply.github.com>
2022-11-11 15:39:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,68 @@
+import os
+
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import MessageEvent, TextMessage, events, TextSendMessage
+
+#Logging
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'WARNING',
+        'handlers': ['wsgi']
+    }
+})
+
+#App Init
+app = Flask(__name__)
+
+ChAccToken = os.environ['CHANNEL_ACCESS_TOKEN']
+ChSecret = os.environ['CHANNEL_SECRET']
+# Channel Access Token
+line_bot_api = LineBotApi(ChAccToken)
+# Channel Secret
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+# 處理訊息
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
